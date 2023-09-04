6f9860adbdc95c930bd439a431daf18771fc54c6
JRossMorgan <jedmorgam@hotmail.com>
2023-09-04 14:06:11 -0600

    2nd Commit

--- src/controller/LogIn.java
+++ src/controller/LogIn.java
@@ -1,13 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LogIn implements Initializable {
+    public TextField userNameTF;
+    public TextField passwordTF;
+    public Button logIn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void onLogIn(ActionEvent actionEvent) {
     }
 }
