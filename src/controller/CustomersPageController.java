696cbfa5b4e56dfa4b32e5f1078cb38e23dde2b3
JRossMorgan <jedmorgam@hotmail.com>
2023-09-24 16:55:32 -0600

    9th Commit

--- src/controller/CustomersPageController.java
+++ src/controller/CustomersPageController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.Customers;
 import model.CustomersDAO;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,6 +27,10 @@ public class CustomersPageController implements Initializable {
     public TableColumn <Customers, String> custPhone;
     public TableColumn <Customers,String> custDivision;
     public TableColumn <Customers, String> custCountry;
+    public Button modCustomer;
+    public Button deleteCustomer;
+    public Button addCustomer;
+    public DialogPane custDialog;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -31,5 +43,19 @@ public class CustomersPageController implements Initializable {
         custPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
         custDivision.setCellValueFactory(new PropertyValueFactory<>("division"));
         custCountry.setCellValueFactory(new PropertyValueFactory<>("country"));
+    }
+
+    public void onMod(ActionEvent actionEvent) {
+    }
+
+    public void onDelete(ActionEvent actionEvent) {
+    }
+
+    public void onAdd(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/AddCustomer.fxml"));
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/model/CustomersDAO.java
+++ src/model/CustomersDAO.java
@@ -7,6 +7,9 @@ import javafx.collections.ObservableList;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class CustomersDAO {
     public static ObservableList<Customers> getAllCustomers(){
@@ -32,5 +35,40 @@ public class CustomersDAO {
             throwables.printStackTrace();
         }
         return allCustomers;
+    }
+    public static void insertCustomer(String custName, String address, String postalCode, String phone, LocalDateTime created, int divisionId){
+        String sql = "insert into customers (Customer_Name, Address, Postal_Code, Phone, Create_Date, Division_ID) values (?, ?, ?, ?, ?, ?)";
+        try{
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            ps.setString(1, custName);
+            ps.setString(2,address);
+            ps.setString(3, postalCode);
+            ps.setString(4, postalCode);
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now(ZoneId.of("GMT"))));
+            ps.setInt(6, divisionId);
+            ps.executeUpdate();
+
+        }
+        catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void updateCustomer(int customerId, String custName, String address, String postalCode, String phone, LocalDateTime created, int divisionId){
+        String sql ="update customers set Customer_Name = ?, Address = ?, Postal_Code = ?, Phone = ?, Create_Date = ?, Division_ID = ? where Customer_ID = ?";
+        try{
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            ps.setString(1, custName);
+            ps.setString(2,address);
+            ps.setString(3, postalCode);
+            ps.setString(4, postalCode);
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now(ZoneId.of("GMT"))));
+            ps.setInt(6, divisionId);
+            ps.setInt(7, customerId);
+            ps.executeUpdate();
+        }
+        catch(SQLException throwables){
+            throwables.printStackTrace();
+        }
     }
 }
